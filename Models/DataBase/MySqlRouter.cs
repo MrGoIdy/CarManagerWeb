0930ec61754acb0aaedcc9391b3ef25573f90700
MrGoIdy <zolotarev.me@mail.ru>
2019-07-05 14:12:36 +0300

    1.0

--- Models/DataBase/MySqlRouter.cs
+++ Models/DataBase/MySqlRouter.cs
@@ -6,25 +6,30 @@ using System.Web;
 
 namespace CarManagerWeb.Models.DataBase
 {
-    public class DataBaseController
+    public class MySqlRouter
     {
         const string SERVER = "5.132.159.203";
         const string USER_ID = "alex";
         const string PASSWORD = "NastiViennaMinsk2018_";
         const string DATABASE = "Test_Alex";
 
-
-
         public static string GetConnectionString()
         {
-            return (new MySqlConnectionStringBuilder
+            try
             {
-                Server = SERVER,
-                UserID = USER_ID,
-                Password = PASSWORD,
-                Database = DATABASE
-            })
-            .ConnectionString;
+                return (new MySqlConnectionStringBuilder
+                {
+                    Server = SERVER,
+                    UserID = USER_ID,
+                    Password = PASSWORD,
+                    Database = DATABASE
+                })
+                .ConnectionString;
+            }
+            catch
+            {
+                throw new ArgumentNullException();
+            }
         }
     }
 }
